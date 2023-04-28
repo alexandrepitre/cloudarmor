efd1ea44c3158a5bc56128f08ba6e0ad62d1e74c
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-04-28 16:01:20 -0400

    dsdffd

--- main.tf
+++ main.tf
@@ -3,4 +3,6 @@ module "cloud_armor" {
   project_id = var.project_id
   name = var.policy_name
   description = var.policy_description
+  layer_7_ddos_defense_enable = true
+  layer_7_ddos_defense_rule_visibility = "STANDARD"
 }
