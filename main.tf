506ad2d6c1dd0187cbb920e961b75730ded5c017
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-05-04 11:57:28 -0400

    fghfgh

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 
-
-
 module "cloud_armor" {
   source = "./modules/cloud-armor"
   project_id = var.project_id
@@ -17,52 +15,109 @@ module "cloud_armor" {
   #Set target to lb backend
   pre_configured_rules = {
 
-    "sqli_sensitivity_level_4" = {
+    "sqli_level4" = {
       action          = "deny(502)"
       priority        = 1
+      description  = "SQL injection sensitivity level 4"
       target_rule_set = "sqli-v33-stable"
+      sensitivity_level  = 4
     }
 
-    "xss-stable_level_2_with_exclude" = {
-      action                  = "throttle"
+    "xss_level2" = {
+      action                  = "deny(502)"
       priority                = 2
-      description             = "XSS Sensitivity Level 2 with excluded rules"
-      preview                 = true
+      description             = "Cross-site scripting sensitivity level 2"
       target_rule_set         = "xss-v33-stable"
       sensitivity_level       = 2
-      exclude_target_rule_ids = ["owasp-crs-v030301-id941380-xss", "owasp-crs-v030301-id941340-xss"]
-      rate_limit_options = {
-        exceed_action                        = "deny(502)"
-        rate_limit_http_request_count        = 10
-        rate_limit_http_request_interval_sec = 60
-      }
     }
 
-    "php-stable_level_1_with_include" = {
-      action                  = "rate_based_ban"
+    "lfi_level1" = {
+      action                  = "deny(502)"
       priority                = 3
-      description             = "PHP Sensitivity Level 1 with included rules"
-      target_rule_set         = "xss-v33-stable"
-      sensitivity_level       = 0
-      include_target_rule_ids = ["owasp-crs-v030301-id933190-php", "owasp-crs-v030301-id933111-php"]
-      exclude_target_rule_ids = []
-      rate_limit_options = {
-        ban_duration_sec                     = 600
-        enforce_on_key                       = "ALL"
-        exceed_action                        = "deny(502)"
-        rate_limit_http_request_count        = 10
-        rate_limit_http_request_interval_sec = 60
-        ban_http_request_count               = 1000
-        ban_http_request_interval_sec        = 300
-      }
+      description             = "Local file inclusion sensitivity level 1"
+      target_rule_set         = "lfi-v33-stable"
+      sensitivity_level       = 1
     }
 
-    "rfi_sensitivity_level_4" = {
-      action          = "redirect"
-      priority        = 4
-      description     = "Remote file inclusion 4"
+    "rce_level3" = {
+      action                  = "deny(502)"
+      priority                = 4
+      description             = "Remote code execution sensitivity level 3"
+      target_rule_set         = "rce-v33-stable"
+      sensitivity_level       = 3
+    }
+
+    "rfi_level2" = {
+      action          = "deny(502)"
+      priority        = 5
+      description     = "Remote file inclusion sensitivity level 2"
       redirect_type   = "GOOGLE_RECAPTCHA"
       target_rule_set = "rfi-v33-stable"
+      sensitivity_level = 2
+    }
+
+    "method-enforcement_level1" = {
+      action = "deny(502)"
+      priority = 6
+      description  = "Method enforcement sensitivity level 1"
+      target_rule_set = "methodenforcement-v33-stable"
+      sensitivity_level = 1
+    }
+
+    "scanner-detection_level2" = {
+      action = "deny(502)"
+      priority = 7
+      description  = "Scanner detection sensitivity level 2"
+      target_rule_set = "scannerdetection-v33-stable"
+      sensitivity_level = 2
+    }
+
+    "protocol-attack_level3" = {
+      action = "deny(502)"
+      priority = 8
+      description  = "Scanner detection sensitivity level 3"
+      target_rule_set = "protocolattack-v33-stable"
+      sensitivity_level = 3
+    }
+
+    "php_level1" = {
+      action                  = "deny(502)"
+      priority                = 9
+      description             = "PHP sensitivity level 1"
+      target_rule_set         = "php-v33-stable"
+      sensitivity_level       = 1
+    }
+
+    "sesion-fixation-level1" = {
+      action                  = "deny(502)"
+      priority                = 10
+      description             = "Session fixation sensitivity level 1"
+      target_rule_set         = "sessionfixation-v33-stable"
+      sensitivity_level       = 1
+    }
+
+    "java-attack-level3" = {
+      action                  = "deny(502)"
+      priority                = 11
+      description             = "Java attack sensitivity level 3"
+      target_rule_set         = "java-v33-stable"
+      sensitivity_level       = 3
+    }
+
+    "nodejs-attack-level1" = {
+      action                  = "deny(502)"
+      priority                = 12
+      description             = "NodeJS attack sensitivity level 1"
+      target_rule_set         = "nodejs-v33-stable"
+      sensitivity_level       = 1
+    }
+
+    "cve-level3" = {
+      action                  = "deny(502)"
+      priority                = 13
+      description             = "CVEs sensitivity level 3"
+      target_rule_set         = "cve-canary"
+      sensitivity_level       = 3
     }
 
   }
